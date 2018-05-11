61b0b57b7440ba8814cb8437b5cca8b6ba7f31d5
nathanestill <nathan.estill@students.olin.edu>
2018-05-10 22:05:21 -0400

    changing Lidar file

--- arduino_code/EncoderCode/EncoderCode.ino
+++ arduino_code/EncoderCode/EncoderCode.ino
@@ -3,7 +3,7 @@
 #include <std_msgs/Float64.h>
 #include <geometry_msgs/Point.h>
 
-Encoder knobLeft(18, 19);
+Encoder WireEncoder(18, 19);
 ros::NodeHandle nh;
 geometry_msgs::Point pnt_msg;
 ros::Publisher hitch_pose("hitch_pose",&pnt_msg);
@@ -19,18 +19,19 @@ void setup() {
   pnt_msg.z = 0.0;
 }
 
-long positionLeft  = -999;
+long positionWireEncoder  = -999;
 
 void loop() {
-  long newLeft;
+  long newWireEncoder;
   float HeightOfHitch;
-  float LeftInInches;
-  newLeft = knobLeft.read();  
-  if (newLeft != positionLeft) {
-    LeftInInches = newLeft / 1000.0;
-    HeightOfHitch = LeftInInches * 1.1429 + 1.7474;
+  float WireEncoderInInches;
+  newWireEncoder = WireEncoder.read(); 
+  if (newWireEncoder != positionWireEncoder) {
+    WireEncoderInInches = newWireEncoder / 1000.0;
+    Serial.println(WireEncoderInInches);
+    HeightOfHitch = WireEncoderInInches * 1.1429 + 1.7474;
     pnt_msg.z = HeightOfHitch;
-    positionLeft = newLeft;
+    positionWireEncoder = newWireEncoder;
   }
   hitch_pose.publish(&pnt_msg);
   nh.spinOnce();
